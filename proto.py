7c56902c002da18c74288fec642498c623ba1ff4
zota <zota@example.com>
2025-05-31 17:20:45 +0900

    fix: state flow

--- proto.py
+++ proto.py
@@ -9,8 +9,9 @@ class StateControl:
         self.cur_spd = 0
         self.spd_lim = 0
 
+        self.state = 0
+
         self.stop_event = threading.Event()
-        self.sign_event = threading.Event()
 
         self.thread = threading.Thread(target=self._loop)
         self.thread.start()
@@ -20,38 +21,48 @@ class StateControl:
         self.thread.join()
 
     def inform_sign(self,new_spd):
+        if self.state == 1:
+            raise("already in input state")
+        if self.state == 2:
+            raise("already in result disp state")
         self.spd_lim = new_spd
         print(f"spd_lim={self.spd_lim}")
-        self.sign_event.set()
+        self.state = 1
 
     def _loop(self):
-        state = 0
         while not self.stop_event.is_set():
 
-            if state == 0:
+            if self.state == 0:
                 print("main")
                 time.sleep(0.5)
 
-            elif self.sign_event.is_set():
-                state = 1
+            elif self.state == 1:
                 # sign found
-                self.sign_event.clear()
                 print("start input state")
                 tw = TimeoutWatcher(3)
                 while not tw.is_timeout():
                     print("running")
                     time.sleep(0.3)
                 print("done")
-            elif state == 2:
-                pass
+                self.state = 2
+
+            elif self.state == 2:
+                # result disp
+                print("start result disp state")
+                tw = TimeoutWatcher(1)
+                while not tw.is_timeout():
+                    print("running")
+                    time.sleep(0.3)
+                print("done")
+                self.state = 0
 
 stc = StateControl()
 
 time.sleep(2)
 stc.inform_sign(30)
-time.sleep(3)
+time.sleep(5)
 stc.inform_sign(50)
-time.sleep(3)
+time.sleep(5)
 
 stc.stop()
 pass
