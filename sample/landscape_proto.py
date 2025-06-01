b910ad6692a3273795678067317d8a8d26593240
zota <zota@example.com>
2025-06-01 20:44:23 +0900

    add: train

---
+++ sample/landscape_proto.py
@@ -0,0 +1,100 @@
+import pygame
+import threading
+import time
+
+WIDTH,HEIGHT = 640,480
+
+FPS = 30
+
+class Landscape:
+    def __init__(self,screen):
+        self.screen = screen
+        self.pos = (200,180)
+        self.height = 50
+        self.length = 163
+
+    def _wheel(self,x):
+        wheel_d = 10
+        pos = self.pos[0] + x, self.pos[1] + self.height + wheel_d
+        pygame.draw.circle(self.screen, (100,100,130), pos, wheel_d)
+        pos = self.pos[0] + x + 22, self.pos[1] + self.height + wheel_d
+        pygame.draw.circle(self.screen, (100,100,130), pos, wheel_d)
+
+        x += (self.length - (wheel_d*2 + 22))
+        pos = self.pos[0] + x, self.pos[1] + self.height + wheel_d
+        pygame.draw.circle(self.screen, (100,100,130), pos, wheel_d)
+        pos = self.pos[0] + x + 22, self.pos[1] + self.height + wheel_d
+        pygame.draw.circle(self.screen, (100,100,130), pos, wheel_d)
+
+    def _car(self):
+        self._wheel(10)
+        rect_size = (self.length, self.height)
+        pygame.draw.rect(self.screen, (0,180,30), pygame.Rect(*self.pos,*rect_size), 100)
+
+    def draw(self):
+        self._car()
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("電車シミュレータ")
+        self.clock = pygame.time.Clock()
+
+        self.landscp = Landscape(self.screen)
+
+        self.running = True
+        self.speed = 0
+
+    def run(self):
+        while self.running:
+            self.clock.tick(FPS)
+            self.handle_events()
+            self.update()
+            self.draw()
+
+        self.cleanup()
+        print("cleanup")
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_q]:
+            self.running = False
+
+        if keys[pygame.K_UP]:
+            self.speed += 1
+            time.sleep(0.03)
+            if self.speed > 100:
+                self.speed = 100
+        elif keys[pygame.K_DOWN]:
+            self.speed -= 1
+            time.sleep(0.03)
+            if self.speed < 0:
+                self.speed = 0
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+
+        self.landscp.draw()
+
+        pygame.display.flip()
+
+    def cleanup(self):
+        pygame.quit()
+        print("quit")
+        sys.exit()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+
